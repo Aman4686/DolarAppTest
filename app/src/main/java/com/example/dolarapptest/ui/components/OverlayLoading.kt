e0eddf312c4de4d179daeeb417bd66b09e62c662
Aman4686 <MrAman@ukr.net>
2026-05-30 14:24:10 +0200

    add default components: LoadingScreen, OverlayLoading

---
+++ app/src/main/java/com/example/dolarapptest/ui/components/OverlayLoading.kt
@@ -0,0 +1,22 @@
+package com.example.dolarapptest.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun OverlayLoading() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable(enabled = false) {},
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
